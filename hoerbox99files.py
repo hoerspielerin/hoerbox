1b85930086b65f414a7d5b42618e811bb7aa0d1c
hoerspielerin <38436531+hoerspielerin@users.noreply.github.com>
2018-04-16 20:13:56 +0200

    Update hoerbox99files.py

--- hoerbox99files.py
+++ hoerbox99files.py
@@ -1,1 +1,27 @@
 hello world
+---------------notes----------------------
+
+cvlc 11.ogg --play-and-exit
+
+killall vlc - abort
+
+if os.path.isfile("/home/user/music/11111111.ogg"):
+   print "file existing"
+else:
+   print "file not exiting"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+------------------------------------------
