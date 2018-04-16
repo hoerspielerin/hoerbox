66f4ca41ed295007cc14194422bf344064de75bc
hoerspielerin <38436531+hoerspielerin@users.noreply.github.com>
2018-04-16 21:33:37 +0200

    Update hoerbox99files.py

--- hoerbox99files.py
+++ hoerbox99files.py
@@ -11,17 +11,32 @@ else:
    print "file not exiting"
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 ------------------------------------------
+
+
+
+
+
+------------------input-string-manipulation------------------
+path = "/home/user/music/"
+file_number = 111
+file_ending = ".ogg"
+actualString = path + str(file_number) + file_ending
+print actualString
+-------------------------------------------------------------
+
+--------------check-existing-file----------------------------
+if os.path.isfile(actualString):
+	print "1 - Got a true expression value"
+else:
+   print "if is not true"
+-------------------------------------------------------------   
+
+ 
+
+
+
+
+
+
+
